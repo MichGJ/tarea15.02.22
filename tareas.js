ffbaffc23978b8b33897522a5236ee7f5a2df90a
MichGJ <98841946+MichGJ@users.noreply.github.com>
2022-02-17 07:54:51 -0600

    agrgeue la tarea para hoy

---
+++ tareas.js
@@ -0,0 +1,98 @@
+
+//EJEJRCICIO 1
+
+let x = prompt( "Dame primer numero");
+console.log(typeof x);
+
+let y = prompt( "Dame segundo numero");
+console.log(typeof y);
+
+
+let suma = parseFloat(x) + parseFloat(y);
+
+
+alert("La suma es " + suma );
+console.log(suma);
+
+//EDAD y dtaos personales
+
+let edad = prompt( "Escribe tu edad");
+console.log(typeof edad);
+alert("Tu edad es " + edad );
+console.log(edad);
+
+//nombre
+
+let nombre = prompt( "Escribe tu nombre");
+console.log(typeof nombre);
+alert("Tu nombre es " + nombre);
+console.log(nombre);
+
+//direccion
+
+let direccion = prompt( "Escribe tu direccion");
+console.log(typeof direccion);
+alert("Tu direccion es " + direccion );
+console.log(direccion);
+
+//PRECIO BOLILLO
+let bolillo = prompt( "Escribe precio de bolillo");
+console.log(typeof bolillo);
+let precioBolillo = parseFloat(bolillo);
+alert("El precio del bolillo es " + bolillo );
+console.log(precioBolillo);
+
+
+//nacimiento
+
+let birthday = prompt( "Escribe tu fecha de nacimiento");
+console.log(typeof birthday);
+let date = parseFloat(birthday);
+alert("Tu fecha de nacimiento  es " + birthday );
+console.log(date);
+
+//INE
+
+let ine = prompt( "Escribe tu clave de elector");
+console.log(typeof ine );
+alert("Tu INE es " + ine );
+console.log(ine);
+
+//mascota
+let pet = prompt( "Escribe el nombre de tu mascota");
+console.log(typeof pet );
+alert("El nombre de tu mascota es " + pet );
+console.log(pet);
+
+
+//AREA DE UN TRIANGULO
+let base = prompt( "Escribe la medida de la base del triangulo");
+console.log (base);
+let altura = prompt( "Escribe la medida de la altura del triangulo");
+console.log(altura);
+let area= (parseFloat(altura)*parseFloat(base))/2;
+alert("El área del triangulo es " + area );
+
+//Perimetro de un cuadrado
+
+let ladoCuadro=prompt("ingresa el largo de uno de los lados del cuadro");
+console.log (ladoCuadro);
+let perimetro=(ladoCuadro*4);
+console.log(perimetro);
+alert("El perimetro de tu cuadrado es: " + perimetro);
+
+
+//empresa
+
+let saco=(1560/1.18);
+console.log(saco);
+alert("El precio del saco con descuento es: "+saco);
+
+let saco2=(1560/1.10);
+console.log(saco2);
+alert("El precio del saco con descuento es: "+saco2);
+
+
+
+
+
